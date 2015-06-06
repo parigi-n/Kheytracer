426a8d7ed4a36e90a8b1f3dcf7f9b292b5ee9b11
Oscar Nosworthy <oscar.nosworthy@epitech.eu>
2015-06-06 17:01:10 +0200

    tam

---
+++ src/raytracer/triangle_tools.c
@@ -0,0 +1,39 @@
+/*
+** triangle_tools.c for tools in /home/noswor_o/MUL_2014_rtracer/oscar
+** 
+** Made by Oscar Nosworthy
+** Login   <noswor_o@epitech.net>
+** 
+** Started on  Sat Jun  6 11:25:32 2015 Oscar Nosworthy
+** Last update Sat Jun  6 16:15:02 2015 Oscar Nosworthy
+*/
+
+#include "struct.h"
+#include "rt.h"
+
+t_coor  cross_product(t_coor point1, t_coor point2)
+{
+  t_coor        product;
+  product.x = (point1.y * point2.z) - (point1.z * point2.y);
+  product.y = (point1.x * point2.z) - (point1.z * point2.x);
+  product.z = (point1.y * point2.x) - (point1.x * point2.y);
+  return (product);
+}
+
+double  dot_product(t_coor point1, t_coor point2)
+{
+  double        dot;
+
+  dot = point1.x * point2.x + point1.y * point2.y + point1.z * point2.z;
+  return (dot);
+}
+
+t_coor          minus(t_coor point1, t_coor point2)
+{
+  t_coor        vector;
+
+  vector.x = point1.x - point2.x;
+  vector.y = point1.y - point2.y;
+  vector.z = point1.z - point2.z;
+  return (vector);
+}
